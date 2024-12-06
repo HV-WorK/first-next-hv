dc403beb04cb41037e2bb452f2c41a5f8de6f861
learning hv <hetvyasdomain@gmail.con>
2024-12-06 18:04:13 +0530

    github commit 1

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            hello het patel {" "}
+            sujay and the prashant and the prince  {" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               src/app/page.js
             </code>
