ced434086a9cc24468f31bd37461f72a0549dd73
learning hv <hetvyasdomain@gmail.con>
2024-12-06 14:17:18 +0530

    this is the seond change

--- src/app/page.js
+++ src/app/page.js
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li> i am gonnna see this  </li>
+          <li> hello rutwik ajsdkgasdfgpaiusd kaam </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
